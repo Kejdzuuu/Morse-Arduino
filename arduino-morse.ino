601dd2e407e9143623bb1bd9ed57fbfdf58369ea
Kejdzu <kejdzuuu@gmail.com>
2018-03-04 01:42:47 +0100

    Added source code

---
+++ arduino-morse.ino
@@ -0,0 +1,91 @@
+#define LED 8
+#define kropka 200
+
+struct Character {
+  char letter;
+  char morse_rep[6];
+};
+
+
+Character alphabet[] = 
+{
+  {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+  {'F', "..-."}, {'G', "-.."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+  {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+  {'P', ".--."}, {'R', ".-."}, {'S', "..."}, {'T', "-"}, {'U', "..-"},
+  {'W', ".--"}, {'V', "...-"}, {'X', "-..-"}, {'Y', "-.-"}, {'Z', "--.."},
+  {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"}, {'5', "....."},
+  {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."}, {'0', "-----"},
+  {' ', " "}, {NULL, NULL}
+};
+
+char toupper(char letter) {
+  if (letter >= 'a' && letter <= 'z') return (letter - ('a' - 'A')); // return uppercase
+  if (letter >= 'A' && letter <= 'Z') return letter;
+  if (letter >= '0' && letter <= '9') return letter;
+  if (letter == ' ') return letter;
+  return NULL;
+}
+
+int find_letter(char l) {
+  int i = 0;
+  char letter = toupper(l);
+  if (letter == NULL) return -1;
+  while (alphabet[i].letter != NULL) {
+    if (alphabet[i].letter == letter) {
+      return i;
+    }
+    i++;
+  }
+  return -1;
+}
+
+void blink_LED(char c){
+  if(c == '-'){
+    digitalWrite(LED, HIGH);
+    delay(3*kropka);
+    digitalWrite(LED, LOW);
+  }
+  if(c == '.'){
+    digitalWrite(LED, HIGH);
+    delay(kropka);
+    digitalWrite(LED, LOW);
+  }
+  delay(3*kropka);
+}
+
+void print_letter(char letter) {
+  int index = find_letter(letter);
+  if (index == -1) {
+    return;;
+  }
+  int i = 0;
+  while (alphabet[index].morse_rep[i] != '\0') {
+    Serial.println(alphabet[index].morse_rep[i]); 
+    blink_LED(alphabet[index].morse_rep[i]);
+    i++;
+  }
+  //printf("\t%c\n", alphabet[index].letter);
+  delay(7*kropka);
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+  Serial.begin(9600); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String tekst;
+  if(Serial.available() > 0){
+    tekst = Serial.readStringUntil('\n');
+    int i = 0;
+    while (tekst[i] != '\0') {
+      print_letter(tekst[i]);
+      i++;
+    }
+  }
+}
